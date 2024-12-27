284afaa0f1c1c4b7a2566ce8fcb852887d478884
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2024-12-27 04:14:02 +0400

    first

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,439 +1,113 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     gsap.registerPlugin(ScrollTrigger);
-//
-//     const initAnimation = () => {
-//
-//         gsap.fromTo(
-//             '.peculiarities-item--one',
-//             { rotation: 12 }, // Начальный наклон
-//             {
-//                 scrollTrigger: {
-//                     trigger: '.peculiarities-item--one', // Триггер для первой карточки
-//                     start: 'top 80%', // Начать выравнивание чуть раньше
-//                     end: 'top center', // Завершить выравнивание
-//                     scrub: true,
-//                 },
-//                 rotation: 0,
-//
-//             }
-//         );
-//         gsap.to('.peculiarities-item--one', {
-//             scrollTrigger: {
-//                 trigger: '.peculiarities-item--one', // Триггером будет сама карточка
-//                 start: 'top center', // Анимация начинается, когда карточка достигает центра экрана
-//                 end: '+=500', // Длительность анимации на скролле
-//                 scrub: true, // Связываем скролл с анимацией
-//
-//             },
-//             xPercent: -200,
-//             yPercent: -130,
-//             ease: 'power1.inOut',
-//             rotation: -25,
-//             duration:2
-//
-//         });
-//
-//         // Анимация для второй карточки
-//         gsap.to('.peculiarities-item--two', {
-//             scrollTrigger: {
-//                 trigger: '.peculiarities-item--two', // Триггер для второй карточки
-//                 start: 'top center-=300', // Начать анимацию немного позже (на 200px)
-//                 end: '+=500',
-//                 scrub: true,
-//             },
-//             xPercent: 100,
-//             yPercent: -130,
-//             ease: 'power1.inOut',
-//             rotation: 25,
-//             duration: 2,
-//         });
-//
-//     };
-//
-//     initAnimation();
-//
-//     // Перезапуск анимации при изменении размеров экрана
-//     window.addEventListener('resize', () => {
-//         ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-//         initAnimation();
-//     });
-// });
 
+gsap.registerPlugin(ScrollTrigger);
 
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    const initAnimation = () => {
-        const timeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.pin-content',
-                start: 'top 10%', // Начало анимации
-                end: '+=700', // Продолжительность скролла
-                scrub: true, // Плавность анимации при скролле
-                pin: true, // Закрепляем секцию
-                anticipatePin: 1,
-            }
-        });
-        gsap.fromTo(
-            '.peculiarities-item--one',
-            { rotation: 12 },
-            {
-                scrollTrigger: {
-                    trigger: '.peculiarities-item--one',
-                    start: 'top 80%',
-                    end: 'top center',
-                    scrub: true,
-                },
-                rotation: 0,
-            }
-        );
-        gsap.fromTo(
-            '.peculiarities-item--two',
-            { opacity: 0 },
-            {
-                scrollTrigger: {
-                    trigger: '.peculiarities-item--two',
-                    start: 'top 50%',
-                    end: 'top center',
-                    scrub: true,
-                    ease: 'power1.inOut',
-                    duration: 2,
-                },
-                opacity: 2,
-            }
-        );
-
-        gsap.fromTo(
-            '.peculiarities-item--tree',
-            { opacity: 0 },
-            {
-                scrollTrigger: {
-                    trigger: '.peculiarities-item--tree',
-                    start: 'top 50%',
-                    end: 'top center',
-                    scrub: true,
-                    ease: 'power1.inOut',
-                    duration: 2,
-                },
-                opacity: 2,
-            }
-        );
-
-
-        gsap.to('.peculiarities-item--one', {
-            scrollTrigger: {
-                trigger: '.peculiarities-item--one', // Триггером будет сама карточка
-                start: 'top center', // Анимация начинается, когда карточка достигает центра экрана
-                end: '+=500', // Длительность анимации на скролле
-                scrub: true, // Связываем скролл с анимацией
-
-            },
-            xPercent: -250,
-            yPercent: -130,
-            ease: 'power1.inOut',
-            rotation: -25,
-            duration:2
-
-        });
-
-        gsap.to('.peculiarities-item--two', {
-            scrollTrigger: {
-                trigger: '.peculiarities-item--two', // Триггер для второй карточки
-                start: 'top center-=300', // Начать анимацию немного позже (на 200px)
-                end: '+=500',
-                scrub: true,
-            },
-            xPercent: 150,
-            yPercent: -130,
-            ease: 'power1.inOut',
-            rotation: 25,
-            duration: 2,
-
-        });
-    };
-
-    ScrollTrigger.matchMedia({
-        "(max-width: 768px)": () => {
-            const timeline = gsap.timeline({
-                scrollTrigger: {
-                    trigger: '.pin-content',
-                    start: 'top top', // Начало анимации на самом верху
-                    end: '+=300', // Продолжительность скролла
-                    scrub: true,
-                    pin: true, // Закрепляем секцию
-                    anticipatePin: 1,
-                    // markers: true, // Для дебага можно добавить маркеры
-                }
-            });
-
-            // Первая карточка на мобильных устройствах
-            gsap.fromTo('.peculiarities-item--one',
-                { rotation: 0 },
-                {
-                    scrollTrigger: {
-                        trigger: '.peculiarities-item--one',
-                        start: 'top center',
-                        end: '+=500',
-                        scrub: true,
-                    },
-                    rotation: 0,
-                }
-            );
-
-            gsap.to('.peculiarities-item--one', {
-                scrollTrigger: {
-                    trigger: '.peculiarities-item--one',
-                    start: 'top center',
-                    end: '+=500',
-                    scrub: true,
-                },
-                xPercent: -180,
-                yPercent: -130,
-                ease: 'power1.inOut',
-                rotation: -25,
-                duration: 2
-            });
-
-            // Вторая карточка на мобильных устройствах
-            gsap.to('.peculiarities-item--two', {
-                scrollTrigger: {
-                    trigger: '.peculiarities-item--two',
-                    start: 'top center-=300',
-                    end: '+=500',
-                    scrub: true,
-                },
-                xPercent: 80,
-                yPercent: -130,
-                ease: 'power1.inOut',
-                rotation: 25,
-                duration: 2,
-            });
-        },
-
-        // Для десктопных версий
-        "(min-width: 769px)": () => {
-            initAnimation(); // Запуск анимации для десктопа
-        }
-    });
-
-    window.addEventListener('resize', () => {
-        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-        initAnimation();
-    });
-});
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    const initAnimationFace = () => {
-        const timeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.face-pin-content',
-                start: 'top 10%',
-                end: '+=700',
-                scrub: true,
-                pin: true,
-                anticipatePin: 1,
-            }
-        });
-
-        gsap.to('.face-peculiarities-item--one', {
-            scrollTrigger: {
-                trigger: '.face-peculiarities-item--one',
-                start: 'top center',
-                end: '+=500',
-                scrub: true,
-            },
-            yPercent: -250,
-            ease: 'power1.inOut',
-            duration: 2,
-        });
-
-        // gsap.fromTo(
-        //     '.face-peculiarities-item--tree',
-        //     { opacity: 0 },
-        //     {
-        //         scrollTrigger: {
-        //             trigger: '.face-peculiarities-item--tree', // Триггер для первой карточки
-        //             start: 'top 20%', // Начать выравнивание чуть раньше
-        //             end: 'top center', // Завершить выравнивание
-        //             scrub: true,
-        //             ease: 'power1.inOut',
-        //
-        //         },
-        //         opacity: 1,
-        //     }
-        // );
-
-        gsap.to('.face-peculiarities-item--tree', {
-            scrollTrigger: {
-                trigger: '.face-peculiarities-item--tree',
-                start: 'top center-=300',
-                end: '+=500',
-                scrub: true,
-            },
-            yPercent: -250,
-            ease: 'power1.inOut',
-            duration: 2,
-        });
-
-
-        // gsap.fromTo(
-        //     '.face-peculiarities-item--two',
-        //     { opacity: 0 },
-        //     {
-        //         scrollTrigger: {
-        //             trigger: '.face-peculiarities-item--tree', // Триггер для первой карточки
-        //             start: 'top 10%', // Начать выравнивание чуть раньше
-        //             end: 'top center', // Завершить выравнивание
-        //             scrub: true,
-        //             ease: 'power1.inOut',
-        //
-        //         },
-        //         opacity: 1,
-        //     }
-        // );
-
-        gsap.to('.face-peculiarities-item--two', {
-            scrollTrigger: {
-                trigger: '.face-peculiarities-item--tree',
-                start: 'top center-=600',
-                end: '+=500',
-                scrub: true,
-            },
-            yPercent: -250,
-            ease: 'power1.inOut',
-            duration: 2,
-        });
-    };
-
-    const initAnimationFaceMobile = () => {
-        const timeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.face-pin-content',
-                start: 'top 10%',
-                end: '+=300',
-                scrub: true,
-                pin: true,
-                anticipatePin: 1,
-            }
-        });
-
-        gsap.to('.face-peculiarities-item--one', {
-            scrollTrigger: {
-                trigger: '.face-peculiarities-item--one',
-                start: 'top center',
-                end: '+=300',
-                scrub: true,
-            },
-            yPercent: -250,
-            ease: 'power1.inOut',
-            duration: 2,
-        });
-
-
-        gsap.to('.face-peculiarities-item--tree', {
-            scrollTrigger: {
-                trigger: '.face-peculiarities-item--tree',
-                start: 'top center-=200',
-                end: '+=300',
-                scrub: true,
-            },
-            yPercent: -250,
-            ease: 'power1.inOut',
-            duration: 2,
-        });
-
-
-
-        gsap.to('.face-peculiarities-item--two', {
-            scrollTrigger: {
-                trigger: '.face-peculiarities-item--two',
-                start: 'top center-=400',
-                end: '+=300',
-                scrub: true,
-            },
-            yPercent: -250,
-            ease: 'power1.inOut',
-            duration: 2,
-        });
-    };
-
-
-    ScrollTrigger.matchMedia({
-        "(min-width: 769px)": () => {
-            // const animationTriggers = initAnimationFace(); // Сохраняем триггеры
-            initAnimationFace()
-            ScrollTrigger.matchMedia({
-                "(max-width: 768px)": () => {
-                    // Удаляем только триггеры `initAnimationFace`
-                    // animationTriggers.forEach(trigger => trigger.kill());
-
-
-                }
-            });
-        },
-        "(max-width: 768px)": () => {
-            ScrollTrigger.matchMedia({
-                "(max-width: 768px)": () => {
-                    initAnimationFaceMobile()
-                }
-            });
-
-        }
-    });
-    window.addEventListener('resize', () => {
-        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-        initAnimationFace();
-    });
-
-
+// Анимация элемента
+gsap.to(".hero-parallax-image ", {
+    y: 150,
+    scrollTrigger: {
+        trigger: ".poetry-hero-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    },
 });
 
 
 
+// Анимация элемента
+gsap.to(".industry-parallax-img ", {
+    y: 250,
+    scrollTrigger: {
+        trigger: ".industry-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    },
+});
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    gsap.registerPlugin(ScrollTrigger);
+// Анимация элемента
+gsap.to(".verse-parallax--one", {
+    x: -250,
+    scrollTrigger: {
+        trigger: ".first-verse-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        // markers: true, // Включены маркеры для проверки
+    },
+});
+gsap.to(".verse-parallax--two", {
+    x: -250,
+    scrollTrigger: {
+        trigger: ".first-verse-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    },
+});
 
-    const initAnimationUsage = () => {
-        if (window.innerWidth > 1000) {
-            gsap.fromTo(
-                '.usage-item',
-                {
-                    scale: 3.5,
-                    opacity: 0,
-                    y: -300,
-                    z: -200,
-                },
-                {
-                    scale: 1,
-                    opacity: 1,
-                    y: 0,
-                    z: 0,
-                    duration: 1,
-                    ease: 'power3.out',
-                    stagger: 0.2,
-                    scrollTrigger: {
-                        trigger: '.usage-section',
-                        start: 'top 20%',
-                        end: 'top bottom',
-                        toggleActions: 'play none none reverse',
-                    },
-                }
-            );
-        }
-    };
+gsap.to(".verse-parallax--for", {
+    x: 250,
+    scrollTrigger: {
+        trigger: ".first-verse-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    },
+});
+gsap.to(".verse-parallax--tree", {
+    x: 200,
+    scrollTrigger: {
+        trigger: ".first-verse-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    },
+});
+gsap.to(".advantages-parallax--one", {
+    y: 200,
+    scrollTrigger: {
+        trigger: ".advantages-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    },
+});
+gsap.to(".rewriting-hero-parallax", {
+    y: 200,
+    scrollTrigger: {
+        trigger: ".rewriting-hero-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    },
+});
+gsap.to(".presentation-hero-parallax", {
+    y: 200,
+    scrollTrigger: {
+        trigger: ".presentation-hero-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    },
+});
+gsap.to(".synonymizer-hero-parallax", {
+    y: -200,
+    scrollTrigger: {
+        trigger: ".synonymizer-hero-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    },
+});
 
-    initAnimationUsage();
-
-    window.addEventListener('resize', () => {
-        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-        initAnimationUsage();
-    });
+gsap.to(".presentation-advantages-parallax", {
+    y: -200,
+    scrollTrigger: {
+        trigger: ".presentation-advantages-section",
+        start: "top 70%",
+        end: "bottom top",
+        scrub: true,
+    },
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,5 @@
 
 
-$(document).ready(function () {
-    $(".possibilities-slider-picture").twentytwenty();
-});
-
-
 
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
@@ -67,26 +62,28 @@ window.addEventListener('scroll', function () {
 
 
 
-
 function toggleActiveState(item) {
-    const allItems = document.querySelectorAll('.often-asks-item');
+    const allItems = document.querySelectorAll('.faq-item');
 
     allItems.forEach(otherItem => {
-        if (otherItem !== item) {
-            otherItem.classList.remove('active');
+        if (otherItem !== item && otherItem.classList.contains('active')) {
+            otherItem.classList.remove('active'); // Закрываем остальные
         }
     });
 
-    item.classList.toggle('active');
+    item.classList.toggle('active'); // Открываем или закрываем текущий
 }
 
 document.querySelectorAll('.faq-item').forEach(item => {
     item.addEventListener('click', () => toggleActiveState(item));
+
     const icon = item.querySelector('.faq-item-icon');
-    icon.addEventListener('click', (event) => {
-        event.stopPropagation();
-        toggleActiveState(item);
-    });
+    if (icon) {
+        icon.addEventListener('click', (event) => {
+            event.stopPropagation(); // Предотвращаем срабатывание родительского клика
+            toggleActiveState(item);
+        });
+    }
 });
 
 
@@ -117,233 +114,32 @@ setupGifToggle('.video-block');
 
 
 
-document.querySelectorAll('.video-block').forEach(block => {
-    const playButton = block.querySelector('.playButton');
-    const videoPlayer = block.querySelector('.videoPlayer');
-    const defaultImage = block.getAttribute('data-gif');
-    let isPlaying = false;
-
-    function playVideo() {
-        block.style.backgroundImage = "none";
-        if (videoPlayer) {
-            videoPlayer.style.display = "block";
-        }
-        videoPlayer.play();
-        if (playButton) {
-            playButton.style.display = "none";
-
-        }
-        isPlaying = true;
-    }
-
-    function stopVideo() {
-        block.style.backgroundImage = `url('${defaultImage}')`;
-        if (videoPlayer) {
-            videoPlayer.style.display = "none";
-            videoPlayer.pause();
-            videoPlayer.currentTime = 0;
-        }
-
-        if (playButton) {
-            playButton.style.display = "block";
-
-        }
-        isPlaying = false;
-    }
-
-
-    if (playButton) {
-        playButton?.addEventListener("click", () => {
-            isPlaying ? stopVideo() : playVideo();
-        });
-    }
-    if (block) {
-        block?.addEventListener("click", () => {
-            isPlaying ? stopVideo() : playVideo();
-        });
-    }
-
-    if (videoPlayer) {
-        videoPlayer.addEventListener("ended", stopVideo);
-    }
-});
-
-
-
-document.querySelectorAll('.gradient-hover').forEach(btn => {
-    const iconVoice = btn.parentElement.querySelector('.icon-voice');
-    const mainActions = btn.closest('.gradient-hover');
-
-    btn.addEventListener('mouseenter', () => {
-        if (iconVoice) {
-            iconVoice.classList.add('icon-hover');
-        }
-        mainActions.classList.add('active');
-
-    });
-
-    btn.addEventListener('mouseleave', () => {
-        if (iconVoice) {
-            iconVoice.classList.remove('icon-hover');
-        }
-        mainActions.classList.remove('active');
-
-    });
-});
-
-
-handleScroll();
-
-function handleScroll() {
-    const elements = document.querySelectorAll('.animation');
-    elements.forEach(element => {
-        const elementRect = element.getBoundingClientRect();
-        const isElementVisible = elementRect.top < window.innerHeight && elementRect.bottom >= 0;
-
-        if (isElementVisible) {
-            if (!element.classList.contains('active')) {
-                element.classList.add('active');
-            }
-        } else {
-            if (element.classList.contains('active')) {
-                element.classList.remove('active');
-            }
-        }
-    });
-}
-
-window.addEventListener('scroll', handleScroll);
-
-
-
-document.querySelectorAll('.avatar-cards').forEach((column) => {
-    column.addEventListener('mouseover', () => {
-        column.style.animationPlayState = 'paused';
-    });
-
-    column.addEventListener('mouseout', () => {
-        column.style.animationPlayState = 'running';
-    });
-});
-
-document.querySelectorAll('.peculiarities-column--one').forEach((column) => {
-    column.addEventListener('mouseover', () => {
-        column.style.animationPlayState = 'paused';
-    });
-
-    column.addEventListener('mouseout', () => {
-        column.style.animationPlayState = 'running';
-    });
-});
-
-
-document.querySelectorAll('.peculiarities-column--two').forEach((column) => {
-    column.addEventListener('mouseover', () => {
-        column.style.animationPlayState = 'paused';
-    });
-
-    column.addEventListener('mouseout', () => {
-        column.style.animationPlayState = 'running';
-    });
-});
-
-document.querySelectorAll('.peculiarities-column--tree').forEach((column) => {
-    column.addEventListener('mouseover', () => {
-        column.style.animationPlayState = 'paused';
-    });
-
-    column.addEventListener('mouseout', () => {
-        column.style.animationPlayState = 'running';
-    });
-});
-
-
-$(document).ready(function () {
-    $(".avatar-hero-style .avatar-style-item").hide();
-    $(".avatar-hero-style .avatar-style-item[data-id='original']").show();
-    $(".avatar-card").on("click", function () {
-        const selectedId = $(this).attr("data-id");
-        $(".avatar-hero-style .avatar-style-item").hide();
-        $(`.avatar-hero-style .avatar-style-item[data-id='${selectedId}']`).show();
-    });
-});
-
 document.addEventListener("DOMContentLoaded", () => {
-    const items = document.querySelectorAll(".photo-tools-item");
-    const videos = document.querySelectorAll(".photo-tools-video");
+    const tabs = document.querySelectorAll(".possibilities-tab");
+    const contents = document.querySelectorAll(".possibilities-cnt");
 
-    // Изначально активируем первый элемент
-    if (items.length > 0 && videos.length > 0) {
-        items[0].classList.add("active");
-        videos.forEach((video, index) => {
-            video.style.display = index === 0 ? "block" : "none"; // Показываем только первое видео
-        });
+    // Функция переключения активных элементов
+    function activateTab(tab) {
+        const targetId = tab.getAttribute("data-id");
+
+        // Удаляем активный класс у всех вкладок и контента
+        tabs.forEach(t => t.classList.remove("active"));
+        contents.forEach(c => c.classList.remove("active"));
+
+        // Добавляем активный класс для выбранной вкладки и контента
+        tab.classList.add("active");
+        document.querySelector(`.possibilities-cnt[data-id="${targetId}"]`).classList.add("active");
     }
 
-    items.forEach(item => {
-        item.addEventListener("click", () => {
-            const selectedId = item.getAttribute("data-id");
-
-            // Удаляем класс active у всех элементов
-            items.forEach(i => i.classList.remove("active"));
-            videos.forEach(video => video.style.display = "none");
-
-            item.classList.add("active");
-
-            const videoToShow = document.querySelector(`.photo-tools-video[data-id="${selectedId}"]`);
-            if (videoToShow) {
-                videoToShow.style.display = "block";
-            }
-        });
+    // Назначаем обработчики событий для всех вкладок
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => activateTab(tab));
     });
+
+    // Изначально активировать первый элемент
+    activateTab(tabs[0]);
 });
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const mockup = document.querySelector(".improve-hero-mockup");
-    const cards = document.querySelectorAll(".improve-hero-card img");
 
-    // Проверка, находится ли элемент в блоке мокапа
-    function isInMockup(element) {
-        const rect = element.getBoundingClientRect();
-        const mockupRect = mockup.getBoundingClientRect();
-        return (
-            rect.top < mockupRect.bottom &&
-            rect.bottom > mockupRect.top &&
-            rect.left < mockupRect.right &&
-            rect.right > mockupRect.left
-        );
-    }
 
-    // Функция для замены изображений
-    function updateImages() {
-        cards.forEach((img) => {
-            if (isInMockup(img) && img.dataset.highSrc) {
-                img.src = img.dataset.highSrc; // Меняем src на high-quality
-                img.dataset.highSrc = ""; // Убираем, чтобы повторно не менять
-            }
-        });
-        requestAnimationFrame(updateImages); // Постоянно вызываем проверку
-    }
-
-    // Запуск постоянного рендера
-    updateImages();
-});
-
-document.querySelectorAll('.video-block').forEach(block => {
-    const videoPlayer = block.querySelector('.videoPlayer');
-
-    function playVideo() {
-        if (videoPlayer) {
-            videoPlayer.play().catch(error => {
-                console.error('Ошибка воспроизведения видео:', error);
-            });
-        }
-    }
-
-    videoPlayer.addEventListener('canplay', playVideo);
-
-    videoPlayer.addEventListener('ended', playVideo);
-
-    videoPlayer.addEventListener('pause', playVideo);
-});
